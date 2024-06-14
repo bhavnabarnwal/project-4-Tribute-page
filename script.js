c58e07eaed0474326b5bca133996d2731b979818
bhavnabarnwal <bhavnabarnwal1234@gmail.com>
2024-06-14 21:46:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var typed = new Typed(".typing",{
+    strings: ["An Inspiration", "Helper", "Feeds People", "Theosophist","Nobel price for peace", "Poor People's God", "Reformer",],
+    typeSpeed:100,
+    backSpeed:80,
+    loop:true
+});
